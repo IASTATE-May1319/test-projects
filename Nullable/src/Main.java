bf571a33fb825e852a3bc8ec59ac6bb751ee587a
Brian Reber <reber.brian@gmail.com>
2013-02-12 20:35:28 -0600

    Fix indentation

--- Nullable/src/Main.java
+++ Nullable/src/Main.java
@@ -5,58 +5,57 @@ import annotations.Nullable;
 
 public class Main {
 
-    @Nullable
-    String testField = null;
-    @NonNull
-    String testField2 = null;
-    @Metric
-    static double speedMetersPerSecond = 11.2;
-    @Metric
-    static double distanceToMarsInMeters = 1241251.92;
-    @English
-    static double speedMilesPerHour = 18536.023;
-    @English
-    @SuppressWarnings(value = { "TypeChecking" })
-    static double distanceToMarsInMiles = 342343;
-
-    public void a() {
 	@Nullable
-	String str = "str";
-	c(str);
-    }
+	String testField = null;
+	@NonNull
+	String testField2 = null;
+	@Metric
+	static double speedMetersPerSecond = 11.2;
+	@Metric
+	static double distanceToMarsInMeters = 1241251.92;
+	@English
+	static double speedMilesPerHour = 18536.023;
+	@English
+	@SuppressWarnings(value = { "TypeChecking" })
+	static double distanceToMarsInMiles = 342343;
 
-    public String b() {
-	String s = null;
-	return s;
-    }
+	public void a() {
+		@Nullable String str = "str";
+		c(str);
+	}
 
-    public void c(@NonNull String data) {
-	// Some random code that assumes data is not null
+	public String b() {
+		String s = null;
+		return s;
+	}
 
-	testField2 = testField;
-    }
+	public void c(@NonNull String data) {
+		// Some random code that assumes data is not null
 
-    public double time(@Metric double distance, @Metric double speed) {
-	return distance / speed;
-    }
+		testField2 = testField;
+	}
 
-    public static void main(@NonNull String[] args) {
-	Main m = new Main();
-	String temp = m.testField;
-	Object o = temp;
-	// m.a();
-	// m.c("test");
-	// m.c(m.b());
+	public double time(@Metric double distance, @Metric double speed) {
+		return distance / speed;
+	}
 
-	String fromB = m.b();
-	// temp = m.testField;
-	m.c(fromB);
+	public static void main(@NonNull String[] args) {
+		Main m = new Main();
+		String temp = m.testField;
+		Object o = temp;
+		// m.a();
+		// m.c("test");
+		// m.c(m.b());
 
-	m.c((String) o);
-	double time = m.time(distanceToMarsInMeters, speedMilesPerHour);
-	time = m.time(distanceToMarsInMiles, speedMetersPerSecond);
-	time = m.time(distanceToMarsInMiles, speedMilesPerHour);
-	time = m.time(distanceToMarsInMeters, speedMetersPerSecond);
-	System.out.println(time);
-    }
+		String fromB = m.b();
+		// temp = m.testField;
+		m.c(fromB);
+
+		m.c((String) o);
+		double time = m.time(distanceToMarsInMeters, speedMilesPerHour);
+		time = m.time(distanceToMarsInMiles, speedMetersPerSecond);
+		time = m.time(distanceToMarsInMiles, speedMilesPerHour);
+		time = m.time(distanceToMarsInMeters, speedMetersPerSecond);
+		System.out.println(time);
+	}
 }
